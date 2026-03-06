84207a56bc10404fb5792c8eea21d36e5d64ac4c
manoob977-svg <manoob977@gmail.com>
2026-03-06 16:54:27 +0500

    Implement PWA for mobile app support

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'home-food-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/app_icon_192.png',
+    '/app_icon_512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
